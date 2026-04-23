8af5c6d3c4333b4dcfa795c4d70b3f392dcd147e
SandySmashHub <sandypc22h2@gmail.com>
2026-04-24 00:54:27 +0530

    fix token final

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 // src/api.js
 import axios from 'axios';
 
-const API = axios.create({ baseURL: '/api' });
+const API = axios.create({ baseURL: 'https://smart-relief-backend.onrender.com/api' });
 
 API.interceptors.request.use(cfg => {
   const t = localStorage.getItem('srToken');
